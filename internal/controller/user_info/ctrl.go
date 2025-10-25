16c75f7a96979338956e0dcde372f8b34fc1b659
tangthinker <tangthinker@yeah.net>
2025-10-25 22:59:59 +0800

    feat: add limit

--- internal/controller/user_info/ctrl.go
+++ internal/controller/user_info/ctrl.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/tangthinker/secret-chat-server/helper/response"
+	"github.com/tangthinker/secret-chat-server/internal/middleware"
 	"github.com/tangthinker/secret-chat-server/internal/model/schema"
 	"github.com/tangthinker/secret-chat-server/internal/proto"
 	"github.com/tangthinker/secret-chat-server/internal/service/user_info"
@@ -37,7 +38,8 @@ func (ctrl *Ctrl) UpdateUserInfo(ctx *fiber.Ctx) error {
 	if err := ctx.BodyParser(userInfo); err != nil {
 		return response.Error(ctx, fiber.StatusBadRequest, "Update User Info: Bad Request")
 	}
-
+	uid := ctx.Locals(middleware.UIDKey).(string)
+	userInfo.UID = uid
 	err := ctrl.userInfoService.UpdateUserInfo(ctx.Context(), userInfo)
 	if err != nil {
 		log.Errorf("update user info error: %s", err)
